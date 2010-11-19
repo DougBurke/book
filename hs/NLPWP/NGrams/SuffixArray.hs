4b1bf0335798eee511426b21be7e4931e750c072
Daniël de Kok <me@danieldk.eu>
2010-11-20 00:06:08 +0100

    chap-ngrams: finish the suffix array section (mostly).

--- hs/NLPWP/NGrams/SuffixArray.hs
+++ hs/NLPWP/NGrams/SuffixArray.hs
@@ -44,12 +44,13 @@ binarySearchBy cmp v n = binarySearchByBounded cmp v n 0 (V.length v - 1)
 binarySearchByBounded :: (Ord a) => (a -> a -> Ordering) -> V.Vector a ->
                          a -> Int -> Int -> Maybe Int
 binarySearchByBounded cmp v e lower upper
+    | V.null v      = Nothing
     | upper < lower = Nothing
-    | otherwise = case cmp e (v V.! middle) of
-                    LT -> binarySearchByBounded cmp v e lower (middle - 1)
-                    EQ -> Just middle
-                    GT -> binarySearchByBounded cmp v e (middle + 1) upper
-    where middle = (lower + upper) `div` 2
+    | otherwise     = case cmp e (v V.! middle) of
+                        LT -> binarySearchByBounded cmp v e lower (middle - 1)
+                        EQ -> Just middle
+                        GT -> binarySearchByBounded cmp v e (middle + 1) upper
+    where middle    = (lower + upper) `div` 2
 
 contains :: Ord a => SuffixArray a -> V.Vector a -> Bool
 contains s e = case binarySearch (restrict eLen s) e of
@@ -57,3 +58,68 @@ contains s e = case binarySearch (restrict eLen s) e of
                  Nothing -> False
     where eLen = V.length e
           restrict len = V.map (V.take len) . elems
+
+lowerBoundByBounds :: Ord a => (a -> a -> Ordering) -> V.Vector a -> a ->
+                      Int -> Int -> Maybe Int
+lowerBoundByBounds cmp v e lower upper
+    | V.null v = Nothing
+    | upper == lower = case cmp e (v V.! lower) of
+                         EQ -> Just lower
+                         _  -> Nothing
+    | otherwise = case cmp e (v V.! middle) of
+                    GT -> lowerBoundByBounds cmp v e (middle + 1) upper
+                    _  -> lowerBoundByBounds cmp v e lower middle
+    where middle = (lower + upper) `div` 2
+
+lowerBoundBounds :: Ord a => V.Vector a -> a -> Int -> Int -> Maybe Int
+lowerBoundBounds = lowerBoundByBounds compare
+
+lowerBoundBy :: Ord a => (a -> a -> Ordering) -> V.Vector a -> a -> Maybe Int
+lowerBoundBy cmp v e = lowerBoundByBounds cmp v e 0 (V.length v - 1)
+
+lowerBound :: Ord a => V.Vector a -> a -> Maybe Int
+lowerBound = lowerBoundBy compare
+
+upperBoundByBounds :: Ord a => (a -> a -> Ordering) -> V.Vector a -> a ->
+                      Int -> Int -> Maybe Int
+upperBoundByBounds cmp v e lower upper
+    | V.null v       = Nothing
+    | upper <= lower = case cmp e (v V.! lower) of
+                         EQ -> Just lower
+                         _  -> Nothing
+    | otherwise      = case cmp e (v V.! middle) of
+                         LT -> upperBoundByBounds cmp v e lower (middle - 1)
+                         _  -> upperBoundByBounds cmp v e middle upper
+    where middle     = ((lower + upper) `div` 2) + 1
+
+upperBoundBounds :: Ord a => V.Vector a -> a -> Int -> Int -> Maybe Int
+upperBoundBounds = upperBoundByBounds compare
+
+upperBoundBy :: Ord a => (a -> a -> Ordering) -> V.Vector a -> a -> Maybe Int
+upperBoundBy cmp v e = upperBoundByBounds cmp v e 0 (V.length v - 1)
+
+upperBound :: Ord a => V.Vector a -> a -> Maybe Int
+upperBound = upperBoundBy compare
+
+frequencyByBounds :: Ord a => (a -> a -> Ordering) -> V.Vector a -> a ->
+                     Int -> Int -> Maybe Int
+frequencyByBounds cmp v e lower upper = do
+  lower <- lowerBoundByBounds cmp v e lower upper
+  upper <- upperBoundByBounds cmp v e lower upper
+  return $ upper - lower + 1
+
+frequencyBy :: Ord a => (a -> a -> Ordering) -> V.Vector a -> a ->
+               Maybe Int
+frequencyBy cmp v e = frequencyByBounds cmp v e 0 (V.length v - 1)
+
+frequencyBounds :: Ord a => V.Vector a -> a -> Int -> Int -> Maybe Int
+frequencyBounds = frequencyByBounds compare
+
+frequency :: Ord a => V.Vector a -> a -> Maybe Int
+frequency = frequencyBy compare
+
+containsWithFreq :: Ord a => SuffixArray a -> V.Vector a -> Maybe Int
+containsWithFreq s e = frequency (restrict eLen s) e
+    where eLen = V.length e
+          restrict len = V.map (V.take len) . elems
+
